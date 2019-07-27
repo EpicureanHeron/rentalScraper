f853175570c5d9a9107a658f6f70ff173c5b2261
Joe Thor <joethor1989@gmail.com>
2019-07-27 07:31:34 -0500

    parsing each page's attributes and post description, along with each post's unique ID and if it is a repost, that as well

--- scraper.py
+++ scraper.py
@@ -19,7 +19,29 @@ liArray = soup.find_all("li", class_="result-row".split())
 
 # iterate through the results
 for li in liArray:
+    # get the ID of the post
+    dataid = li['data-pid']
+    # check to see if it is a repost and grab repost id
+    if li.has_attr('data-repost-of'):
+        repost = li['data-repost-of']
+    else:
+        repost = 'None'
+
     # parse the <a> tag
-    link = li.find('a', class_= 'hdrlnk')
-    # prints the link itself
-    print(link['href'])
+    a = li.find('a', class_= 'hdrlnk')
+    # grabs the URL from HREF of the a tag
+    postingLink = a['href']
+    # requests the posting page
+    postingRes = requests.get(postingLink)
+    # creates soup for posting page
+    postingSoup = bs4.BeautifulSoup(postingRes.text, 'html.parser')
+    # grabs attributes
+    attrGroup = postingSoup.find_all('p', class_='attrgroup'.split())
+    # creates an empty list to store attributes
+    attrList = []
+    # puts attributes into list
+    for attr in attrGroup:
+       attrList.append(attr.text)
+    # grabs posting description
+    description = postingSoup.find(id='postingbody')
+
